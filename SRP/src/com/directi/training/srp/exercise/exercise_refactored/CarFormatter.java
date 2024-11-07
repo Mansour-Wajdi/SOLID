14119d5628df0eb48f5dfcd358931f248ffd0194
Wajdi Mansour <104292697+Mansour-Wajdi@users.noreply.github.com>
2024-11-07 16:32:38 +0100

    Create CarFormatter.java

---
+++ SRP/src/com/directi/training/srp/exercise/exercise_refactored/CarFormatter.java
@@ -0,0 +1,15 @@
+package com.directi.training.srp.exercise_refactored;
+
+import java.util.List;
+
+public class CarFormatter
+{
+    public String formatCarNames(List<Car> cars)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : cars) {
+            sb.append(car.getBrand()).append(" ").append(car.getModel()).append(", ");
+        }
+        return sb.length() > 0 ? sb.substring(0, sb.length() - 2) : ""; // Remove trailing comma
+    }
+}
